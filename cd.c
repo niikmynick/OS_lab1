6bfd966c18bd31b020dc96a070b128b2154b6ae3
Nikita <fk724s@mail.ru>
2024-10-27 13:19:03 +0300

    add cd command

---
+++ cd.c
@@ -0,0 +1,23 @@
+//
+// Created by Никита Кобик on 27.10.2024.
+//
+#include <stdio.h>
+#include <unistd.h>
+
+#include "cd.h"
+
+void cmd_cd(const char *path) {
+    if (chdir(path) != 0) {
+        perror("cd");
+    }
+}
+
+// removes getting the argument from a command handler
+void cmd_cd_wrapper(char **args, int argc) {
+    if (argc > 1) {
+        cmd_cd(args[1]);
+
+    } else {
+        printf("cd: missing argument\n");
+    }
+}
---
+++ include/cd.h
@@ -0,0 +1,10 @@
+//
+// Created by Никита Кобик on 27.10.2024.
+//
+
+#ifndef CD_H
+#define CD_H
+
+void cmd_cd_wrapper(char **args, int argc);
+
+#endif //CD_H
