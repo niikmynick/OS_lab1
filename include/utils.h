9fd166278a5b0937062c2a030a74109181218587
Nikita <fk724s@mail.ru>
2024-10-27 14:14:15 +0300

    add utils

--- include/utils.h
+++ include/utils.h
@@ -1,12 +1,20 @@
 //
 // Created by Никита Кобик on 25.10.2024.
 //
-
 #ifndef UTILS_H
-#define UTILS_H
 
 #define MAX_ARGS 128
+#define MAX_COMMAND_LEN 1024
+#define PATH_LEN 1024
 
+// represents command with its name and handler
+typedef struct {
+    const char *name;
+    void (*func)(char **args, int argc);
+} Command;
+
+void print_prompt();
+void execute_command(char *input, const Command *commands);
 int parse_input(char *input, char **args);
 
 #endif
---
+++ src/utils.c
@@ -0,0 +1,88 @@
+//
+// Created by Никита Кобик on 25.10.2024.
+//
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "utils.h"
+
+
+int parse_input(char *input, char **args) {
+    int argc = 0;
+
+    char *token = strtok(input, " ");
+
+    while (token != NULL && argc < MAX_ARGS - 1) {
+        args[argc++] = token;
+        token = strtok(nullptr, " ");
+    }
+
+    args[argc] = nullptr;
+
+    return argc;
+}
+
+
+void print_prompt() {
+    char cwd[PATH_LEN];
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("shell %s > ", cwd);
+
+    } else {
+        // fallback if the current directory cannot be retrieved
+        perror("getcwd");
+        printf("shell > ");
+    }
+}
+
+
+void execute_command(char *input, const Command *commands) {
+    char *args[MAX_ARGS];
+    const int argc = parse_input(input, args);
+
+    if (argc == 0) {
+        return;
+    }
+
+    const pid_t pid = fork();
+    int status;
+
+    switch (pid) {
+        case -1:
+            perror("fork failed");
+        return;
+
+        case 0:  // child process
+            for (int i = 0; commands[i].name != NULL; i++) {
+                if (strcmp(args[0], commands[i].name) == 0) {
+                    if (commands[i].func != NULL) {
+                        const clock_t start_time = clock();
+
+                        commands[i].func(args, argc);
+
+                        const clock_t end_time = clock();
+                        printf("Execution time: %.2fms\n", ((double)(end_time - start_time)) / CLOCKS_PER_SEC * 1000);
+
+                    } else {
+                        // handle exit command
+                        exit(EXIT_SUCCESS);
+                    }
+                    exit(EXIT_SUCCESS);
+                }
+            }
+
+        // command not found
+        fprintf(stderr, "Command not found: %s\n", args[0]);
+        exit(EXIT_FAILURE);
+
+        default:  // parent process
+            if (waitpid(pid, &status, 0) == -1) {
+                perror("waitpid failed");
+            }
+        break;
+    }
+}
